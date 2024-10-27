e551426ac8edb79a123fbf8af74e8bd261ad055c
Tankudoraiba <wojciech.borak@gmail.com>
2024-10-27 02:39:56 +0200

    Adding 404 page route

--- app.py
+++ app.py
@@ -235,6 +235,10 @@ def static_files(filename):
 def robots_txt():
     return send_from_directory('static/files', 'robots.txt', mimetype='text/plain')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.errorhandler(Exception)
 def handle_exception(e):
     logging.error(f"Exception: {e}")
